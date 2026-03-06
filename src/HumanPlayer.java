1b2fa0aa462b8bf88b91b0e204c606d8a2a241b8
Itsmakiiihehey <makiiiicalamcam@gmail.com>
2026-03-06 17:54:31 +0800

    Update HumanPlayer.java

--- src/HumanPlayer.java
+++ src/HumanPlayer.java
@@ -1,2 +1,14 @@
-public class HumanPlayer {
+class HumanPlayer extends Player {
+    Scanner scanner = new Scanner(System.in);
+    public HumanPlayer(String name) { super(name); }
+    public Move chooseMove() {
+        while (true) {
+            System.out.print("Enter rock, paper, scissors or quit: ");
+            String input = scanner.nextLine().toLowerCase();
+            if (input.equals("quit")) return null;
+            Move move = MoveFactory.createMove(input);
+            if (move != null) return move;
+            System.out.println("Invalid input. Try again.");
+        }
+    }
 }
