5b272620787a85b67ded4426c7b4e2406295416f
UnknownUSer <botai7309@gmail.com>
2026-03-06 18:06:35 +0800

    Update Paper.java

--- src/Paper.java
+++ src/Paper.java
@@ -1,2 +1,8 @@
-public class Paper {
+class Paper extends Move {
+    public int compare(Move other) {
+        if (other instanceof Paper) return 0;
+        if (other instanceof Rock) return 1;
+        return -1;
+    }
+    public String getName() { return "Paper"; }
 }
