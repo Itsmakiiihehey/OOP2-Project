b76526e5c5af7e6277718d17c75338b92b2727d0
Itsmakiiihehey <makiiiicalamcam@gmail.com>
2026-03-06 17:52:34 +0800

    Update ComputerPlayer.java

--- src/ComputerPlayer.java
+++ src/ComputerPlayer.java
@@ -1,2 +1,9 @@
-public class ComputerPlayer {
+class ComputerPlayer extends Player {
+    
+    Random random = new Random();
+    public ComputerPlayer(String name) { super(name); }
+    public Move chooseMove() {
+        String[] moves = {"rock", "paper", "scissors"};
+        return MoveFactory.createMove(moves[random.nextInt(3)]);
+    }
 }
