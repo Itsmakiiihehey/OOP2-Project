38a13ef029c0a5a4bd798bac41003ba5d46027ac
Jeff Sacayan <jeffsacayan14@gmail.com>
2026-03-06 18:19:16 +0800

    Update Scissors.java

--- src/Scissors.java
+++ src/Scissors.java
@@ -1,2 +1,8 @@
-public class Scissors {
+class Scissors extends Move {
+    public int compare(Move other) {
+        if (other instanceof Scissors) return 0;
+        if (other instanceof Paper) return 1;
+        return -1;
+    }
+    public String getName() { return "Scissors"; }
 }
