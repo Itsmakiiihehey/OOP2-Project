0683346959058a4ceb32e9899f095348da7e9a01
jezreelsalgados-netizen <jezreelsalgados@gmail.com>
2026-03-06 18:04:18 +0800

    Update MoveFactory.java

--- src/MoveFactory.java
+++ src/MoveFactory.java
@@ -1,2 +1,10 @@
-public class MoveFactory {
+class MoveFactory {
+    public static Move createMove(String type) {
+        switch (type) {
+            case "rock": return new Rock();
+            case "paper": return new Paper();
+            case "scissors": return new Scissors();
+            default: return null;
+        }
+    }
 }
