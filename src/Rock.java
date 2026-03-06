fa83d2c9349cee0f644fc846675b3c58bb8e0e62
UnknownUSer <botai7309@gmail.com>
2026-03-06 18:08:05 +0800

    Update Rock.java

--- src/Rock.java
+++ src/Rock.java
@@ -1,2 +1,8 @@
-public class Rock {
+class Rock extends Move {
+    public int compare(Move other) {
+        if (other instanceof Rock) return 0;
+        if (other instanceof Scissors) return 1;
+        return -1;
+    }
+    public String getName() { return "Rock"; }
 }
