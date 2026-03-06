705c6d10b79e292b482ba0d0ee505554bfc41f0d
Jeff Sacayan <jeffsacayan14@gmail.com>
2026-03-06 18:19:56 +0800

    Update ScoreBoard.java

--- src/ScoreBoard.java
+++ src/ScoreBoard.java
@@ -1,2 +1,14 @@
-public class ScoreBoard {
+class ScoreBoard {
+    private int humanScore = 0;
+    private int computerScore = 0;
+    public void updateScore(RoundResult result) {
+        if (result == RoundResult.PLAYER1_WIN) humanScore++;
+        else if (result == RoundResult.PLAYER2_WIN) computerScore++;
+    }
+    public void display() {
+        System.out.println("Score -> You: " + humanScore + " | Computer: " + computerScore);
+        System.out.println("---------------------------");
+    }
+    public int getHumanScore() { return humanScore; }
+    public int getComputerScore() { return computerScore; }
 }
