957489586f16cd7558d0de76faac077b320ab2c7
Itsmakiiihehey <makiiiicalamcam@gmail.com>
2026-03-06 17:53:48 +0800

    Update Game.java

--- src/Game.java
+++ src/Game.java
@@ -1,2 +1,42 @@
-public class Game {
+class Game {
+
+    private Player human = new HumanPlayer("You");
+
+    private Player computer = new ComputerPlayer("Computer");
+
+    private ScoreBoard scoreBoard = new ScoreBoard();
+
+    private final int WIN_SCORE = 5;
+ 
+    public void start() {
+
+        System.out.println("=== Rock Paper Scissors: First to 5 Wins ===");
+
+        while (scoreBoard.getHumanScore() < WIN_SCORE && scoreBoard.getComputerScore() < WIN_SCORE) {
+
+            Round round = new Round(human, computer);
+
+            RoundResult result = round.play();
+
+            if (result == RoundResult.QUIT) {
+
+                System.out.println("Thanks for playing!");
+
+                return;
+
+            }
+
+            scoreBoard.updateScore(result);
+
+            scoreBoard.display();
+
+        }
+ 
+        if (scoreBoard.getHumanScore() == WIN_SCORE) System.out.println("Congratulations You won the match!");
+
+        else System.out.println("Computer Wins!");
+
+    }
+
 }
+ 
